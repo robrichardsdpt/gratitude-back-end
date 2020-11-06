5d78c8117f9be0e1c8a1de2ae29f39393064404e
robrichardsdpt <robrichardsdpt@gmail.com>
2020-11-06 17:37:49 -0500

    setting up aws

--- gratitude-back-end/settings.py
+++ gratitude-back-end/settings.py
@@ -170,6 +170,8 @@ USE_TZ = True
 # optional package: http://whitenoise.evans.io/en/stable/django.html
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Use the custom user model as the auth user for the admin view
 AUTH_USER_MODEL = 'api.User'
